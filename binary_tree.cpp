c1c548580b5b3a728ae77e959a9d661dea2757ef
byy_q <3303503486@qq.com>
2026-03-06 08:23:31 +0800

    二叉搜索树

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -84,3 +84,106 @@ void postorder(TreeNode *root,vector<int> &res)
 }
 //空间复杂度分析：O(n)，其中n为二叉树的节点数。在最坏情况下，二叉树可能退化成链表，此时递归调用的深度为n，
 //栈空间的使用为O(n)。在平均情况下，二叉树的高度为O(log n)，此时递归调用的深度为O(log n)，栈空间的使用为O(log n)。因此，空间复杂度为O(n)。
+//二叉搜索树
+/*二叉搜索树的定义：对于二叉搜索树中的每个节点，左子树中的所有节点的值都小于该节点的值，右子树中的所有节点的值都大于该节点的值。*/
+ TreeNode *root;
+ /*查找节点*/
+ TreeNode* search(int num)
+ {
+    TreeNode *cur = root;
+    //循环查找，越过叶节点后跳出
+    while(cur != nullptr)
+    {
+        //目标节点在cur的右子树中
+        if(cur ->val <num)
+        cur = cur ->right;
+        //目标节点在cur的左子树中
+        else if(cur->val > num)
+        cur = cur->left;
+        //找到节点，跳出
+        else
+        break;
+    }
+    return cur;
+ }
+ /*插入节点*/
+ void insert(int num)
+ {
+    //如果树为空，则初始化根节点
+    if(root = nullptr)
+    {
+    root = new TreeNode(num);
+    return ;
+    }
+    TreeNode *cur = root,*pre = nullptr;
+    while(cur != nullptr)
+    {
+        //找到重复节点，直接返回
+        if(cur->val == num)
+        return ;
+        pre = cur;
+        //插入位置在cur的左子树
+        if(cur->val > num)
+        cur = cur->left;
+        //插入位置在cur的右子树
+        else
+        cur = cur ->right;
+    }
+    //插入节点
+    TreeNode *node = new TreeNode(num);
+    if(pre ->val > num)
+    pre->left = node;
+    else
+    pre->right = node;
+ }
+/*删除节点*/
+void remove(int num)
+{
+    //树为空，直接返回
+    if(root == nullptr)
+    return ;
+    TreeNode *cur = root, *pre = nullptr;
+    //循环查找，越过叶节点后跳出
+    while(cur != nullptr)
+    {
+        //目标节点在cur的右子树
+        if(cur ->val <num)
+        cur = cur ->right;
+        //目标节点在cur的左子树
+        else if (cur ->val >num)
+        cur =cur ->left;
+        //找到节点，跳出
+        else
+        break;
+    }
+    //未找到目标节点，直接返回
+    if(cur == nullptr)
+    return ;
+    //删除节点为叶节点或仅有一个子节点
+    if(cur ->left == nullptr || cur ->right == nullptr)
+    {
+        TreeNode *child = (cur ->left != nullptr)? cur->left : cur ->right;
+        //删除节点cue
+        if(cur != root)
+        {
+            if(pre ->left == cur)
+            pre ->left = child;
+            else
+            pre-> right = child;
+        }
+        else
+        root = child;
+        delete cur;
+    }
+    //删除节点有两个子节点
+    else
+    {
+        //找到右字数的最小节点，中序遍历
+        TreeNode *temp = cur ->right;
+        while(temp ->left != nullptr)
+        temp = temp ->left;
+        int tempVal = temp ->val;
+        remove(tempVal);
+        cur ->val = tempVal;
+    }
+}
